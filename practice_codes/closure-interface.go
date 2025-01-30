b6ab0dd212a19588e15fcfeab90820355b52155a
Towfiq <tonoy098@gmail.com>
2025-01-30 17:39:23 +0600

    Update closure-interface.go

--- practice_codes/closure-interface.go
+++ practice_codes/closure-interface.go
@@ -1,44 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
+type I interface {
+	M()
+	N()
 }
 
-type Vertex struct {
-	X, Y float64
+type T struct {
+	a string
+	b string
 }
 
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.a, t.b)
 }
-
-type Abser interface {
-	Abs() float64
+func (t T) N() {
+	fmt.Println(t.a, t.b)
 }
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f  // a MyFloat implements Abser
-	fmt.Println(a.Abs())
-	a = &v // a *Vertex implements Abser
-	fmt.Println(a.Abs())
-	
-	// In the following line, v is a Vertex (not *Vertex)
-	// and does NOT implement Abser.
-	//a = v
-	// fmt.Println(a.Abs())
+	var i I = T{"aaaa", "bbbb"}
+	i.M()
+	i.N()
 }
