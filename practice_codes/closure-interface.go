02ff86e0e77a17e4be843840807d80ddb4987081
Towfiq <tonoy098@gmail.com>
2025-01-30 17:04:06 +0600

    Update closure-interface.go

--- practice_codes/closure-interface.go
+++ practice_codes/closure-interface.go
@@ -5,6 +5,24 @@ import (
 	"math"
 )
 
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 type Abser interface {
 	Abs() float64
 }
@@ -22,21 +40,4 @@ func main() {
 	a = v
 
 	fmt.Println(a.Abs())
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
